4d0730fd202b1af3abde37af183b6b3b20ba07e8
hokageCodes <hashsenju94@gmail.com>
2023-05-21 11:54:34 +0100

    reset password is resetting oooo

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,6 +8,29 @@ const User = require('../models/User');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+const crypto = require('crypto');
+
+// Create a transporter instance
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+
+
+function generateResetToken() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters[randomIndex];
+  }
+  return token;
+}
 function generateUniqueToken(email) {
   const timestamp = Date.now().toString();
   const hashedEmail = bcrypt.hashSync(email, 10); // Hash the email for added uniqueness
@@ -17,42 +40,34 @@ function generateUniqueToken(email) {
 }
 
 async function sendPasswordResetEmail(email, resetUrl) {
-  // Create a transporter instance
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+  try {
+    // Compose the email message
+    const message = {
+      from: process.env.FROM_EMAIL,
+      to: email,
+      subject: 'Password Reset',
+      text: `
+        You are receiving this email because you (or someone else) have requested to reset the password for your account.
+        Please click on the following link or paste it into your browser to complete the process:
+        ${resetUrl}
+        If you did not request this, please ignore this email and your password will remain unchanged.
+      `,
+    };
 
-  // Compose the email message
-  const message = {
-    from: process.env.FROM_EMAIL,
-    to: email,
-    subject: 'Password Reset',
-    text: `
-      You are receiving this email because you (or someone else) have requested to reset the password for your account.
-      Please click on the following link or paste it into your browser to complete the process:
-      ${resetUrl}
-      If you did not request this, please ignore this email and your password will remain unchanged.
-    `,
-  };
-
-  // Send the email
-  await transporter.sendMail(message);
+    // Send the email
+    const info = await transporter.sendMail(message);
+    console.log('Email sent:', info.messageId);
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
 }
-
 // Signup route
 router.post(
   '/signup',
   [
     body('email').trim().isEmail().withMessage('Invalid email').normalizeEmail(),
-    body('password')
-      .trim()
-      .isLength({ min: 6 })
-      .withMessage('Password must be at least 6 characters'),
+    body('password').trim().isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ],
   async (req, res) => {
     try {
@@ -72,13 +87,9 @@ router.post(
 
       const user = new User({ email, password: hashedPassword });
 
-      // Generate a unique token
       const token = generateUniqueToken(email);
-
-      // Store the token in the user's database entry
       user.token = token;
 
-      // Save the user
       await user.save();
 
       res.status(201).json({ token });
@@ -105,19 +116,16 @@ router.post(
 
       const { email, password } = req.body;
 
-      // Check if the user exists
       const user = await User.findOne({ email });
       if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
 
-      // Compare the passwords
       const isPasswordMatch = await bcrypt.compare(password, user.password);
       if (!isPasswordMatch) {
         return res.status(401).json({ message: 'Invalid credentials' });
       }
 
-      // Verify the token and return user ID
       res.json({ token: user.token, userId: user._id });
     } catch (error) {
       console.error('Error:', error);
@@ -126,6 +134,7 @@ router.post(
   }
 );
 
+// Reset password route
 router.post(
   '/reset-password',
   [
@@ -138,23 +147,26 @@ router.post(
         return res.status(400).json({ errors: errors.array() });
       }
 
-      const { email, password } = req.body;
+      const { email } = req.body;
 
-      // Find the user with the provided email
       const user = await User.findOne({ email });
       if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
 
-      // Update the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-      user.password = hashedPassword;
+      const resetToken = generateResetToken();
+      user.resetToken = resetToken;
       await user.save();
 
-      res.json({ message: 'Password reset successful' });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ message: 'Internal server error' });
+      const resetUrl = `http://localhost:5173/reset-password?token=${resetToken}`;
+
+        // Send the password reset email
+        await sendPasswordResetEmail(email, resetUrl);
+
+        res.json({ message: 'Password reset email sent' });
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
   }
 );
